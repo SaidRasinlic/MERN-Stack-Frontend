7095216ce27763411044712872b74bd5cf9c85bb
Said Rasinlic <saidrasinlic.jr@gmail.com>
2023-06-26 13:49:23 +0200

    Add file

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import App from './App';
 import store from './app/store';
 import './assets/style/index.css';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
