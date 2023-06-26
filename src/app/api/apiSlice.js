b2138b67a7d6cc5380f9d4ea376fa14d165bb981
Said Rašinlić <saidrasinlic.jr@gmail.com>
2023-06-26 14:37:40 +0200

    Update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { setCredentials } from '../../features/auth/authSlice';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'https://technotes-backend.onrender.com',
+  baseUrl: 'https://mern-technotes-backend.onrender.com',
   credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const { token } = getState().auth;
